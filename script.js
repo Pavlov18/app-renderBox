a84e83c6c96dafbee59c94c86623fc70ba9c61fe
pavlov18 <topor001@mail.ru>
2020-04-11 21:08:46 +0400

    random circle radius end coordinates + timer

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 var $start = document.querySelector('#start')
 var $game = document.querySelector('#game')
+var $time = document.querySelector('#time')
 
-var score = 0;
+var score = 0
+var isGameStarted = false
 
 $start.addEventListener('click', startGame)
 $game.addEventListener('click', handleBoxClick)
@@ -9,13 +11,34 @@ $game.addEventListener('click', handleBoxClick)
 
 // запуск игры (убираем кнопку старт и заливаем фон белым цветом)
 function startGame() {
+    isGameStarted = true
     $game.style.backgroundColor = '#fff'
     $start.classList.add('hide')
+
+    var interval = setInterval(function(){
+        var time = parseFloat($time.textContent)
+        if (time <=0){
+            clearInterval(interval)
+            endGame()
+        //end game
+        } else {
+            $time.textContent = (time - 0.1).toFixed(1)
+        }  
+    }, 100) //обновление таймера каждые 100 милисекунд
+
     renderBox()
+}
+
+function endGame() {
+    isGameStarted = false
 }
 
 // если кликнули по квадрату (проверка, есть ли аттрибут data-box)
 function handleBoxClick(event) {
+    if (!isGameStarted) {
+        return
+    }
+
     if (event.target.dataset.box){
         score++ // увеличение очков при клике на квадрат
         renderBox()
@@ -27,14 +50,23 @@ function handleBoxClick(event) {
 function renderBox() {
     $game.innerHTML = '' // удаление квадрата при клике на него
     var box = document.createElement('div')
-
-    box.style.height = box.style.width = '50px'
+    var boxSize = getRandom(30, 100)
+    var gameSize = $game.getBoundingClientRect() // получение данных игрового поля
+    var maxTop = gameSize.height - boxSize
+    var maxLeft = gameSize.width - boxSize
+ 
+    box.style.height = box.style.width = boxSize+'px'
     box.style.position = 'absolute'
     box.style.backgroundColor = '#000'
-    box.style.top = '50px'
-    box.style.left = '50px'
+    box.style.borderRadius = '50%'
+    box.style.top = getRandom(0, maxTop) + 'px'
+    box.style.left = getRandom(0, maxLeft) +'px'
     box.style.cursor = 'pointer'
     box.setAttribute('data-box', true)
 
     $game.insertAdjacentElement('afterbegin', box)
+}
+
+function getRandom(min, max) { // генерация случайных чисел
+    return Math.floor(Math.random() * (max-min) +min)
 }
